61582cdd300578663f8a2c73f78e78b59effaabb
KIRANVhits <kirantomar.vhits@gmail.com>
2024-11-21 12:47:20 +0530

    Added | Wallet integration with minting functionality

---
+++ src/components/Mint.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { toast } from "react-toastify";
+import { useAppKitProvider, useAppKitAccount } from "@reown/appkit/react";
+import { Contract, ethers } from "ethers";
+import abi from "../IntergrationHelpers/abi.json";
+const TokenAddress = "0x45567Edb0d60dfb814486C9eB72e67014b3E2b6c";
+const Mint = () => {
+  const { address, isConnected } = useAppKitAccount();
+  const { walletProvider } = useAppKitProvider("eip155");
+  const [amount, setAmount] = React.useState(0);
+  async function mint() {
+    if (!isConnected) throw Error("User disconnected");
+
+    const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
+    const signer = await ethersProvider.getSigner();
+    // The Contract object
+    const TokenContract = new Contract(TokenAddress, abi, signer);
+    try {
+      const tx = await TokenContract.mint(amount);
+      await tx.wait();
+      toast.success("Tokens Minted");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+  return (
+    <div>
+      <h1>Mint</h1>
+      <div>
+        <input
+          style={{ marginRight: "10px" }}
+          placeholder="Amount"
+          type="number"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={mint}>Mint</button>
+      </div>
+    </div>
+  );
+};
+
+export default Mint;
